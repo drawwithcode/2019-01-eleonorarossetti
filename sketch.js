339c5c6615378e13aa386fee59c4e38d2c109112
Eleonora Rossetti <eleonora_rossetti@hotmail.it>
2019-09-27 09:53:29 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,10 +2,43 @@ function preload(){
   // put preload code here
 }
 
+
+function keyPressed() {
+if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+}
+
 function setup() {
-  // put setup code here
+
+createCanvas(windowWidth,windowHeight);
+angleMode(DEGREES);
+frameRate(50);
+background(0);
+
+
+
 }
 
 function draw() {
-  // put drawing code here
+
+  strokeWeight(0.5);
+  stroke(
+    lerpColor(
+      color("tomato"),
+
+      color("slateBlue"),
+      frameCount /50
+    )
+  );
+  line(windowWidth / 2 + 250, windowHeight / 2,windowWidth / 2 + cos(frameCount * 3) * 250, windowHeight / 2 + sin(frameCount * 3) * 250);
+
+  if (frameCount == 240) {
+    noLoop();
+  }
+
+  line(windowWidth / 2 - 250, windowHeight / 2,windowWidth / 2 + cos(frameCount * 3) * 250, windowHeight / 2 + sin(frameCount * 3) * 250);
+
+  if (frameCount == 240) {
+    noLoop();
+  }
+
 }
